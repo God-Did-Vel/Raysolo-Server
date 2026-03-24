036338bb8e801d41a959caa6c1f0076720d03cb0
God-Did-Vel <mfonisocletus124@gmail.com>
2026-03-24 19:43:39 +0100

    updated code

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,8 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://fordham-suite-web.vercel.app'
+    'https://fordham-suite-web.vercel.app',
+    'https://fordham-suites-website.vercel.app'
   
   ]
 }));
